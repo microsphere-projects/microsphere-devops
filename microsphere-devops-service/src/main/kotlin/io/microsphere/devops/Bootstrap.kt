dac291945363b518a4659d3edace11f570cca99d
小马哥（mercyblitz） <mercyblitz@gmail.com>
2024-06-20 14:35:36 +0800

    Create Bootstrap.kt

---
+++ microsphere-devops-service/src/main/kotlin/io/microsphere/devops/Bootstrap.kt
@@ -0,0 +1,18 @@
+package io.microsphere.devops
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+/**
+ * TODO
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see TODO
+ * @since 1.0.0
+ */
+@SpringBootApplication
+class Bootstrap
+
+fun main(args: Array<String>) {
+    runApplication<Bootstrap>(*args)
+}
