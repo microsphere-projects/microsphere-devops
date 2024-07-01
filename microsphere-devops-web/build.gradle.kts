3b0470d4aa293d434797ba8399dcdd58f6e1df78
小马哥（mercyblitz） <mercyblitz@gmail.com>
2024-07-01 23:34:35 +0800

    Update the dependencies

--- microsphere-devops-web/build.gradle.kts
+++ microsphere-devops-web/build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("io.github.microsphere-projects:microsphere-spring-boot-actuator")
+    // implementation("io.github.microsphere-projects:microsphere-spring-boot-actuator")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
