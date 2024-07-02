0abf61c1ebd289ae1e2276a4f64ec8cea3578263
小马哥（mercyblitz） <mercyblitz@gmail.com>
2024-07-02 22:15:17 +0800

    Create NamespaceRepository.kt

---
+++ microsphere-devops-repository/src/main/kotlin/io/microsphere/devops/repository/NamespaceRepository.kt
@@ -0,0 +1,14 @@
+package io.microsphere.devops.repository
+
+import io.microsphere.devops.api.application.Namespace
+import org.springframework.data.repository.CrudRepository
+
+/**
+ * Spring Data Repository for {@link Namespace}
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see Namespace
+ * @since 1.0.0
+ */
+interface NamespaceRepository : CrudRepository<Namespace, Long>{
+}
