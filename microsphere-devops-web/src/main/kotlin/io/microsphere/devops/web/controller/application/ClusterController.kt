7e111dfd615d70c54c167d5015c577543d01cf45
小马哥（mercyblitz） <mercyblitz@gmail.com>
2024-07-09 00:17:52 +0800

    Create ClusterController.kt

---
+++ microsphere-devops-web/src/main/kotlin/io/microsphere/devops/web/controller/application/ClusterController.kt
@@ -0,0 +1,39 @@
+package io.microsphere.devops.web.controller.application
+
+import io.microsphere.devops.api.entity.Cluster
+import io.microsphere.devops.repository.ClusterRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * The Spring Web {@link Controller} for {@link Cluster}
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see Cluster
+ * @since 1.0.0
+ */
+@RestController
+@RequestMapping("/api/app")
+class ClusterController(private val clusterRepository: ClusterRepository) {
+
+    @GetMapping("/clusters")
+    fun queryClusters() = clusterRepository.findAll();
+
+    @GetMapping("/cluster/{id}")
+    fun queryCluster(@PathVariable id: Long) = clusterRepository.findByIdOrNull(id);
+
+    @PostMapping("/cluster")
+    fun saveCluster(cluster: Cluster) = clusterRepository.save(cluster);
+
+    @PutMapping("/cluster")
+    fun updateCluster(cluster: Cluster) = saveCluster(cluster);
+
+    @DeleteMapping("/cluster")
+    fun deleteCluster(id: Long) = clusterRepository.deleteById(id);
+}
