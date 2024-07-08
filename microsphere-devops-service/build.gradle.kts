605f42d3f5b0e6b980cef01f0e31b6eea90731f8
小马哥（mercyblitz） <mercyblitz@gmail.com>
2024-07-09 00:17:17 +0800

    Update build.gradle.kts

--- microsphere-devops-service/build.gradle.kts
+++ microsphere-devops-service/build.gradle.kts
@@ -12,22 +12,18 @@ plugins {
 
 dependencies {
 
-    api(platform(libs.spring.boot.dependencies))
-    api(platform(libs.spring.cloud.dependencies))
+    // Internal Project
+    api(project(":microsphere-devops-repository"))
+
+    // Import the BOMs
     api(platform(libs.spring.cloud.alibaba.dependencies))
     api(platform(libs.microsphere.spring.cloud.dependencies))
 
-    implementation(libs.commons.lang3)
-    implementation(libs.log4j2.core)
+    // Spring Cloud Commons
+    api("org.springframework.cloud:spring-cloud-commons")
 
-    implementation(project(":microsphere-devops-repository"))
-
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    // implementation("io.github.microsphere-projects:microsphere-spring-boot-actuator")
-
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    // Nacos Discovery
+    // api("com.alibaba.cloud:spring-cloud-starter-alibaba-nacos-discovery")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
