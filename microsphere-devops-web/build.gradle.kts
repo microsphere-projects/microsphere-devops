da7b328800b796609373e19beaff0f24a778cb39
小马哥（mercyblitz） <mercyblitz@gmail.com>
2024-07-09 00:17:20 +0800

    Update build.gradle.kts

--- microsphere-devops-web/build.gradle.kts
+++ microsphere-devops-web/build.gradle.kts
@@ -8,27 +8,22 @@ plugins {
     // Support convention plugins written in Kotlin. Convention plugins are build scripts in 'src/main' that automatically become available as plugins in the main build.
     id("buildlogic.kotlin-application-conventions")
     id("org.springframework.boot") version "3.3.0"
+    kotlin("plugin.spring") version "1.9.24"
 }
 
 
 dependencies {
 
-    api(platform(libs.spring.boot.dependencies))
-    api(platform(libs.spring.cloud.dependencies))
-    api(platform(libs.spring.cloud.alibaba.dependencies))
-    api(platform(libs.microsphere.spring.cloud.dependencies))
+    implementation(project(":microsphere-devops-service"))
 
-    implementation(libs.commons.lang3)
     implementation(libs.log4j2.core)
 
-    implementation(project(":microsphere-devops-service"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     // implementation("io.github.microsphere-projects:microsphere-spring-boot-actuator")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
