3f614014b5bea3151ab58ee4034048c2d2b2cdca
小马哥（mercyblitz） <mercyblitz@gmail.com>
2024-07-09 00:17:49 +0800

    Update Bootstrap.kt

--- microsphere-devops-web/src/main/kotlin/io/microsphere/devops/web/Bootstrap.kt
+++ microsphere-devops-web/src/main/kotlin/io/microsphere/devops/web/Bootstrap.kt
@@ -1,7 +1,10 @@
 package io.microsphere.devops.web
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 /**
  * Microsphere DevOps Service Bootstrap
@@ -10,6 +13,9 @@ import org.springframework.boot.runApplication
  * @see EnableAutoConfiguration
  * @since 1.0.0
  */
+@EntityScan(basePackages = ["io.microsphere.devops.api.entity"])
+@EnableJpaRepositories(basePackages = ["io.microsphere.devops.repository"])
+@ComponentScan(basePackages = ["io.microsphere.devops.web.controller"])
 @EnableAutoConfiguration
 open class Bootstrap
 
