be850ec584411e5c0b91304b12254d645f70ae40
小马哥（mercyblitz） <mercyblitz@gmail.com>
2024-07-10 00:48:08 +0800

    Update ApplicationInstanceController.kt

--- microsphere-devops-web/src/main/kotlin/io/microsphere/devops/web/controller/application/ApplicationInstanceController.kt
+++ microsphere-devops-web/src/main/kotlin/io/microsphere/devops/web/controller/application/ApplicationInstanceController.kt
@@ -1,7 +1,8 @@
 package io.microsphere.devops.web.controller.applicationInstance
 
 import io.microsphere.devops.api.entity.ApplicationInstance
-import io.microsphere.devops.repository.ApplicationInstanceRepository
+import io.microsphere.devops.service.application.ApplicationInstanceService
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 /**
@@ -17,25 +19,28 @@ import org.springframework.web.bind.annotation.RestController
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
  * @see ApplicationInstance
- * @see ApplicationInstanceRepository
+ * @see ApplicationInstanceService
  * @since 1.0.0
  */
 @RestController
 @RequestMapping("/api/app")
-class ApplicationInstanceController(private val applicationInstanceRepository: ApplicationInstanceRepository) {
+class ApplicationInstanceController(private val applicationInstanceService: ApplicationInstanceService) {
 
     @GetMapping("/instances")
-    fun queryApplicationInstances() = applicationInstanceRepository.findAll();
+    fun queryApplicationInstances(@RequestParam applicationId: Long, pageable: Pageable) =
+        applicationInstanceService.findAllByApplicationId(applicationId, pageable);
 
     @GetMapping("/instance/{id}")
-    fun queryApplicationInstance(@PathVariable id: Long) = applicationInstanceRepository.findByIdOrNull(id);
+    fun queryApplicationInstance(@PathVariable id: Long) = applicationInstanceService.findByIdOrNull(id);
 
     @PostMapping("/instance")
-    fun saveApplicationInstance(@RequestBody applicationInstance: ApplicationInstance) = applicationInstanceRepository.save(applicationInstance);
+    fun saveApplicationInstance(@RequestParam applicationId: Long, @RequestBody applicationInstance: ApplicationInstance) =
+        applicationInstanceService.saveApplicationInstance(applicationId, applicationInstance);
 
     @PutMapping("/instance")
-    fun updateApplicationInstance(@RequestBody applicationInstance: ApplicationInstance) = applicationInstanceRepository.save(applicationInstance);
+    fun updateApplicationInstance(@RequestBody applicationInstance: ApplicationInstance) =
+        applicationInstanceService.updateApplicationInstance(applicationInstance);
 
     @DeleteMapping("/instance/{id}")
-    fun deleteApplicationInstance(@PathVariable id: Long) = applicationInstanceRepository.deleteById(id);
+    fun deleteApplicationInstance(@PathVariable id: Long) = applicationInstanceService.deleteById(id);
 }
