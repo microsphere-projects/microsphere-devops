5bb895c9ce63b315ec7368f681b5a42fb1672403
小马哥（mercyblitz） <mercyblitz@gmail.com>
2024-07-14 01:31:26 +0800

    Create Cluster.java

---
+++ microsphere-devops-service/src/main/java/io/microsphere/nacos/client/v1/naming/model/Cluster.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.microsphere.nacos.client.v1.naming.model;
+
+import io.microsphere.nacos.client.common.model.Model;
+
+import java.util.Map;
+
+/**
+ * Cluster Model
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see Model
+ * @since 1.0.0
+ */
+public class Cluster implements Model {
+
+    private HealthChecker healthChecker;
+
+    private Map<String, String> metadata;
+
+    private String name;
+
+    public HealthChecker getHealthChecker() {
+        return healthChecker;
+    }
+
+    public void setHealthChecker(HealthChecker healthChecker) {
+        this.healthChecker = healthChecker;
+    }
+
+    public Map<String, String> getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(Map<String, String> metadata) {
+        this.metadata = metadata;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
