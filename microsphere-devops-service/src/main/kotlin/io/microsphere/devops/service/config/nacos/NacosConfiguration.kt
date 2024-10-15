8d4d6f57ed026773c5af1907248ac677c10e10f3
Mercy Ma <mercyblitz@gmail.com>
2024-10-15 21:07:05 +0800

    Update NacosConfiguration.kt

--- microsphere-devops-service/src/main/kotlin/io/microsphere/devops/service/config/nacos/NacosConfiguration.kt
+++ microsphere-devops-service/src/main/kotlin/io/microsphere/devops/service/config/nacos/NacosConfiguration.kt
@@ -4,44 +4,43 @@ import com.alibaba.cloud.nacos.NacosDiscoveryProperties
 import com.alibaba.nacos.api.common.Constants.DEFAULT_NAMESPACE_ID
 import io.microsphere.devops.api.entity.Cluster
 import io.microsphere.devops.api.entity.Namespace
-import io.microsphere.devops.service.application.ApplicationServiceFacade
-import io.microsphere.devops.service.config.nacos.NacosConfiguration.ApplicationConfig
-import org.springframework.cloud.client.discovery.event.InstancePreRegisteredEvent
+import io.microsphere.devops.condition.ConditionalOnNacosProfile
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Profile
-import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.util.StringUtils.hasText
 
-@Profile("nacos")
+@ConditionalOnNacosProfile
 @Configuration(proxyBeanMethods = false)
-@Import(value = [ApplicationConfig::class])
-class NacosConfiguration {
+class NacosConfiguration(val nacosDiscoveryProperties: NacosDiscoveryProperties) {
 
-    class ApplicationConfig(
-        val nacosDiscoveryProperties: NacosDiscoveryProperties,
-        var applicationServiceFacade: ApplicationServiceFacade
-    ) {
+    @Bean
+    fun cluster(): Cluster {
+        val url = nacosDiscoveryProperties.serverAddr;
+        val clusterType = Cluster.Type.NACOS;
+        val userName = nacosDiscoveryProperties.username;
+        val password = nacosDiscoveryProperties.password;
+        var cluster = Cluster(clusterType.name + " Cluster", clusterType, url, userName, password);
+        cluster.description = clusterType.description;
+        return cluster;
+    }
 
-        @EventListener(InstancePreRegisteredEvent::class)
-        fun onInstanceRegisteredEvent(event: InstancePreRegisteredEvent) {
-            val url = nacosDiscoveryProperties.serverAddr;
-            val clusterType = Cluster.Type.NACOS;
-            val userName = nacosDiscoveryProperties.username;
-            val password = nacosDiscoveryProperties.password;
+    @Bean
+    fun namespace(cluster: Cluster): Namespace {
+        // Namespace
+        var ns = nacosDiscoveryProperties.namespace;
+        if (!hasText(ns)) {
+            ns = DEFAULT_NAMESPACE_ID;
+        }
+        return Namespace(ns, Namespace.Status.ACTIVE, cluster);
+    }
 
-            // Cluster
-            var cluster = Cluster(clusterType.name + " Cluster", clusterType, url, userName, password);
-            cluster.description = clusterType.description;
+    @Profile("standalone")
+    class StandaloneConfig {
 
-            // Namespace
-            var ns = nacosDiscoveryProperties.namespace;
-            if (!hasText(ns)) {
-                ns = DEFAULT_NAMESPACE_ID;
-            }
-            var namespace = Namespace(ns, Namespace.Status.ACTIVE, cluster);
-
-            applicationServiceFacade.saveOrUpdate(cluster, namespace, event.registration);
+        @Scheduled(fixedRate = 60 * 1000)
+        fun sync() {
         }
     }
 
