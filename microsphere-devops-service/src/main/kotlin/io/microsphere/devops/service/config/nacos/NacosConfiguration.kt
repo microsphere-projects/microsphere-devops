72098da13c38d9a201ff7144d64f01b368480b78
Mercy Ma <mercyblitz@gmail.com>
2024-10-18 18:58:30 +0800

    Update NacosConfiguration.kt

--- microsphere-devops-service/src/main/kotlin/io/microsphere/devops/service/config/nacos/NacosConfiguration.kt
+++ microsphere-devops-service/src/main/kotlin/io/microsphere/devops/service/config/nacos/NacosConfiguration.kt
@@ -5,10 +5,11 @@ import com.alibaba.nacos.api.common.Constants.DEFAULT_NAMESPACE_ID
 import io.microsphere.devops.api.entity.Cluster
 import io.microsphere.devops.api.entity.Namespace
 import io.microsphere.devops.condition.NacosProfile
+import io.microsphere.devops.condition.StandaloneProfile
 import io.microsphere.devops.repository.ClusterRepository
+import io.microsphere.devops.service.nacos.NacosService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.util.StringUtils.hasText
 
@@ -39,14 +40,17 @@ class NacosConfiguration(
         return Namespace(ns, Namespace.Status.ACTIVE, cluster);
     }
 
-    @Profile("standalone")
+    @StandaloneProfile
+    @Configuration
     class StandaloneConfig(
-        val clusterRepository: ClusterRepository
+        val clusterRepository: ClusterRepository,
+        val nacosService: NacosService
     ) {
 
-        @Scheduled(fixedRate = 60 * 1000)
+        @Scheduled(fixedRate = 10 * 1000)
         fun sync() {
             val nacosClusters = clusterRepository.findAllByType(Cluster.Type.NACOS);
+            nacosService.initClusters(nacosClusters);
         }
     }
 
