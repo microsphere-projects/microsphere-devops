a9f2fc1386528dc159442e50b52b267331750e8a
Mercy Ma <mercyblitz@gmail.com>
2024-10-21 20:21:49 +0800

    Create EntityListener.java

---
+++ microsphere-spring-orm/src/main/java/io/microsphere/jpa/event/EntityListener.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.microsphere.jpa.event;
+
+import jakarta.persistence.EntityListeners;
+
+/**
+ * The lifecycle listener of Entity
+ *
+ * @param <E> the type of Entity
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see EntityListeners
+ * @since 1.0.0
+ */
+public interface EntityListener<E> {
+
+    default void onPostLoad(E entity) {
+    }
+
+    default void onPrePersist(E entity) {
+    }
+
+    default void onPostPersist(E entity) {
+    }
+
+    default void onPreUpdate(E entity) {
+    }
+
+    default void onPostUpdate(E entity) {
+    }
+
+    default void onPreRemove(E entity) {
+    }
+
+    default void onPostRemove(E entity) {
+    }
+}
