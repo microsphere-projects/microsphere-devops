aefdea9e5985c568267c1964745db506d14db45f
Mercy Ma <mercyblitz@gmail.com>
2024-10-22 19:29:48 +0800

    Update User.java

--- microsphere-spring-orm/src/test/java/io/microsphere/entity/User.java
+++ microsphere-spring-orm/src/test/java/io/microsphere/entity/User.java
@@ -18,6 +18,8 @@ package io.microsphere.entity;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 /**
  * User Entity
  *
@@ -66,6 +68,19 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 
     @Override
