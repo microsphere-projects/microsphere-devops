2d9b29b8a6da0f7b33e173efca3723b9570ca4e1
Mercy Ma <mercyblitz@gmail.com>
2024-10-25 15:39:36 +0800

    Update NacosProfile.java

--- microsphere-devops-service/src/main/java/io/microsphere/devops/condition/NacosProfile.java
+++ microsphere-devops-service/src/main/java/io/microsphere/devops/condition/NacosProfile.java
@@ -2,7 +2,11 @@ package io.microsphere.devops.condition;
 
 import org.springframework.context.annotation.Profile;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * {@link Profile} for "nacos"
