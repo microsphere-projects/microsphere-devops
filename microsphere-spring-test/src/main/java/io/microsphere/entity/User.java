2aa69a1cfd804c764f8945fee4424e4df7f2cf8e
Mercy Ma <mercyblitz@gmail.com>
2024-10-25 11:36:19 +0800

    Update User.java

--- microsphere-spring-test/src/main/java/io/microsphere/entity/User.java
+++ microsphere-spring-test/src/main/java/io/microsphere/entity/User.java
@@ -16,10 +16,8 @@
  */
 package io.microsphere.entity;
 
-import io.microsphere.jpa.event.LoggingEntityListener;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -36,9 +34,6 @@ import java.util.Objects;
  */
 @Entity
 @Table(name = "users")
-@EntityListeners(value = {
-        LoggingEntityListener.class
-})
 public class User {
 
     /**
