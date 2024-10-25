801eca8c144bcb8f2dd0afea7470f17dd2e5e3c8
Mercy Ma <mercyblitz@gmail.com>
2024-10-25 15:39:45 +0800

    Update Bootstrap.kt

--- microsphere-devops-web/src/main/kotlin/io/microsphere/devops/web/Bootstrap.kt
+++ microsphere-devops-web/src/main/kotlin/io/microsphere/devops/web/Bootstrap.kt
@@ -1,17 +1,13 @@
 package io.microsphere.devops.web
 
-import io.microsphere.devops.web.config.DefaultConfiguration
 import io.microsphere.devops.web.config.defaultBeans
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.support.beans
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-import org.springframework.scheduling.annotation.AsyncAnnotationBeanPostProcessor.DEFAULT_TASK_EXECUTOR_BEAN_NAME
 import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
 
 /**
  * Microsphere DevOps Service Bootstrap
