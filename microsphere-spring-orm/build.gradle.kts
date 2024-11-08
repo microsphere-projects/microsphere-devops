edbea6d0099791a3771b627ff83b2135e13f5648
Mercy Ma <mercyblitz@gmail.com>
2024-11-08 14:32:26 +0800

    Update build.gradle.kts

--- microsphere-spring-orm/build.gradle.kts
+++ microsphere-spring-orm/build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
 
     // Testing
     testImplementation(project(":microsphere-spring-test"))
+    testImplementation("org.hibernate.orm:hibernate-testing")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
