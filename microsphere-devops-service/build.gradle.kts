0da3cc30a0e004c4b020ee0ea3e66e42b9c8cb9b
Mercy Ma <mercyblitz@gmail.com>
2024-11-15 14:24:22 +0800

    Update build.gradle.kts

--- microsphere-devops-service/build.gradle.kts
+++ microsphere-devops-service/build.gradle.kts
@@ -49,4 +49,5 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("ch.qos.logback:logback-classic")
 }
